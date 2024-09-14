a79d4f5178b35127a5a6ca6109dd5af3e7d816a5
Leonardo Vieira <leonardovieira_94@hotmail.com>
2024-09-14 12:07:57 -0300

    Added Delete method

--- APICatalog/Controllers/ProductsController.cs
+++ APICatalog/Controllers/ProductsController.cs
@@ -64,5 +64,20 @@ namespace APICatalog.Controllers
             return Ok(product);
         }
 
+        [HttpDelete("{id:int}")]
+        public ActionResult Delete(int id)
+        {
+            var product = _context.Products.FirstOrDefault(p => p.ProductId == id);
+
+            if (product is null)
+            {
+                return NotFound("product not found");
+            }
+            _context.Products.Remove(product);
+            _context.SaveChanges();
+
+            return Ok(product);
+        }
+
     }
 }
